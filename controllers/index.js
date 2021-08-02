e1f00b7666a159dcda0a3d9f31373a1f92a8ab1b
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-02 15:05:28 -0500

    created homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+//use of home-routes
+const homeRoutes = require('./home-routes');
+
+router.use('/', homeRoutes);
+
 router.use('/api', apiRoutes);
 router.use((req, res) => {
     res.status(404).end();
