972078e6100e0eefc478ee0a2c559674ac94bb12
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-05 15:41:06 -0500

    added delete function

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 //get all users
 router.get('/', (req, res) => {
@@ -73,12 +74,12 @@ router.get('/:id', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     //expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -88,7 +89,7 @@ router.post('/', (req, res) => {
 });
 
 //PUT /api/posts/upvote
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     // Vote.create({
     //     user_id: req.body.user_id,
     //     post_id: req.body.post_id
@@ -126,7 +127,7 @@ router.put('/upvote', (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title
@@ -150,7 +151,7 @@ router.put('/:id', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,4 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+}
+
