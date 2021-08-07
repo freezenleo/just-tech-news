d70b6eea02e31d962b43b7427052543fd21be386
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-06 22:15:41 -0500

    updated connection file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
-    secret: 'Super secret secret',
+    secret: process.env.DB_SECRET,
     cookie: {},
     resave: false,
     saveUninitialized: true,
