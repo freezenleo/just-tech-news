671a994bd76b74a637930847c817f2bd886cb10c
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-06 22:25:49 -0500

    write down sess secret

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
